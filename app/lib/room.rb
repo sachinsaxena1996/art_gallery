3c05774d77fc27027152f1e5a4bb3c91c9240526
sachinsaxena1996 <sachinsaxena1996@gmail.com>
2017-07-02 20:39:20 +0530

    for rails 5 lib is moved inside app directory

---
+++ app/lib/room.rb
@@ -0,0 +1,37 @@
+# this class represents the  room visited
+class Room
+  attr_accessor :visits
+
+  def initialize(visits)
+    @visits = visits
+  end
+
+  def print_info
+    "Room #{room_id}, #{average_time} minute average visit,
+    #{total_visitors} visitor \n"
+  end
+
+  private
+
+  def room_id
+    visits.first.room
+  end
+
+  def average_time
+    (total_time / total_visitors).to_i
+  end
+
+  def total_time
+    in_time = 0
+    out_time = 0
+    visits.each do |visit|
+      in_time += visit.time if visit.status == 'I'
+      out_time += visit.time if visit.status == 'O'
+    end
+    (out_time - in_time).to_i
+  end
+
+  def total_visitors
+    visits.group_by(&:visitor).count.to_i
+  end
+end
